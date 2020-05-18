b917fe41bd18ecabdda62ab4931b17b0755230b3
osvetnik-game <osvetnik.game@gmail.com>
2020-05-18 10:52:40 +0200

    Init

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public GameObject muzzleFlash;
+    public Transform muzzleFlashPosition;
+    public Transform impact;
+
+    public AudioClip shootSound;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        
+    }
+
+    void Shoot()
+    {
+        GameObject muzzleFlashObject =  Instantiate(muzzleFlash, muzzleFlashPosition.position, muzzleFlashPosition.rotation);
+        muzzleFlashObject.transform.SetParent(gameObject.transform);
+
+        audioSource.PlayOneShot(shootSound);
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            Transform impactObject = Instantiate(impact, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactObject.gameObject, 2f);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController characterController;
+
+    public float moveSpeed = 12f;
+
+    float gravity = -9.81f;
+    Vector3 velocity;
+    public bool isGrounded;
+    public float jumpHeight = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gravity = -9.81f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = characterController.isGrounded;
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.right * x) + (transform.forward * z);
+        characterController.Move(move * moveSpeed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
